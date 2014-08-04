409b17754b877a26e2ab8bf14e18a0eb21155bf5
David Ding <chengjie.ding@gmail.com>
2014-08-04 22:09:20 +0800

    concrete class that extends ColdDrink and Burger class

---
+++ Builder/src/main/java/com/demo2do/patterns/builder/Pepsi.java
@@ -0,0 +1,13 @@
+package com.demo2do.patterns.builder;
+
+public class Pepsi extends ColdDrink {
+    @Override
+    public String name() {
+        return "Pepsi";
+    }
+
+    @Override
+    public float price() {
+        return 35.0f;
+    }
+}
