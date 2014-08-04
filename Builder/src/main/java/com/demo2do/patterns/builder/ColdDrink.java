d2fa3afb4c3883ec4321611279a6f3bb6627c055
David Ding <chengjie.ding@gmail.com>
2014-08-04 22:02:36 +0800

    abstract class implementing Item interface which provide default functions

---
+++ Builder/src/main/java/com/demo2do/patterns/builder/ColdDrink.java
@@ -0,0 +1,11 @@
+package com.demo2do.patterns.builder;
+
+public abstract class ColdDrink implements Item {
+    @Override
+    public Packing packing() {
+        return new Bottle();
+    }
+
+    @Override
+    public abstract float price();
+}
