c5f6a7def3d277e123b702dd2ff191e22f468cee
David Ding <chengjie.ding@gmail.com>
2014-08-04 21:58:38 +0800

    concrete class that implements Packing interface

---
+++ Builder/src/main/java/com/demo2do/patterns/builder/Bottle.java
@@ -0,0 +1,8 @@
+package com.demo2do.patterns.builder;
+
+public class Bottle implements Packing {
+    @Override
+    public String pack() {
+        return "Bottle";
+    }
+}
---
+++ Builder/src/main/java/com/demo2do/patterns/builder/Wrapper.java
@@ -0,0 +1,8 @@
+package com.demo2do.patterns.builder;
+
+public class Wrapper implements Packing {
+    @Override
+    public String pack() {
+        return "Wrapper";
+    }
+}
