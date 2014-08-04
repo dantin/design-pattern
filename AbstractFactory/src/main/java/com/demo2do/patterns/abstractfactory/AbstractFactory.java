b6c1003ec029b4ab2f4e59f63a67abb3eb25e931
David Ding <chengjie.ding@gmail.com>
2014-08-04 14:29:56 +0800

    abstract factory class

---
+++ AbstractFactory/src/main/java/com/demo2do/patterns/abstractfactory/AbstractFactory.java
@@ -0,0 +1,6 @@
+package com.demo2do.patterns.abstractfactory;
+
+public abstract class AbstractFactory {
+    public abstract Color getColor(String color);
+    public abstract Shape getShape(String shape) ;
+}
