4ae3bcf030729e92b2848ca535581715ce0966ee
David Ding <chengjie.ding@gmail.com>
2014-08-04 22:17:40 +0800

    test class which show builder pattern

---
+++ Builder/src/main/java/com/demo2do/patterns/test/BuilderPatternDemo.java
@@ -0,0 +1,20 @@
+package com.demo2do.patterns.test;
+
+import com.demo2do.patterns.builder.Meal;
+import com.demo2do.patterns.builder.MealBuilder;
+
+public class BuilderPatternDemo {
+    public static void main(String[] args) {
+        MealBuilder mealBuilder = new MealBuilder();
+
+        Meal vegMeal = mealBuilder.prepareVegMeal();
+        System.out.println("Veg Meal");
+        vegMeal.showItems();
+        System.out.println("Total Cost: " +vegMeal.getCost());
+
+        Meal nonVegMeal = mealBuilder.prepareNonVegMeal();
+        System.out.println("\n\nNon-Veg Meal");
+        nonVegMeal.showItems();
+        System.out.println("Total Cost: " +nonVegMeal.getCost());
+    }
+}
