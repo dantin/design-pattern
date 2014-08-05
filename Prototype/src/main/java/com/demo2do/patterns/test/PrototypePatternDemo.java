20145c598f6b9850b469f93d9b03b9c000fcd8c1
David Ding <chengjie.ding@gmail.com>
2014-08-06 07:27:53 +0800

    add test class

---
+++ Prototype/src/main/java/com/demo2do/patterns/test/PrototypePatternDemo.java
@@ -0,0 +1,20 @@
+package com.demo2do.patterns.test;
+
+import com.demo2do.patterns.prototype.Shape;
+import com.demo2do.patterns.prototype.ShapeCache;
+
+public class PrototypePatternDemo {
+
+    public static void main(String[] args) {
+        ShapeCache.loadCache();
+
+        Shape clonedShape = ShapeCache.getShape("1");
+        System.out.println("Shape : " + clonedShape.getType());
+
+        Shape clonedShape2 = ShapeCache.getShape("2");
+        System.out.println("Shape : " + clonedShape2.getType());
+
+        Shape clonedShape3 = ShapeCache.getShape("3");
+        System.out.println("Shape : " + clonedShape3.getType());
+    }
+}
