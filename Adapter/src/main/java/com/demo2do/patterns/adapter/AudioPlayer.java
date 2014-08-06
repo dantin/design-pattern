ed4f738001d0ed2212ddc4ae45df0ea15b083131
David Ding <chengjie.ding@gmail.com>
2014-08-06 08:08:32 +0800

    add concrete class that implements MediaPlayer interface

---
+++ Adapter/src/main/java/com/demo2do/patterns/adapter/AudioPlayer.java
@@ -0,0 +1,21 @@
+package com.demo2do.patterns.adapter;
+
+public class AudioPlayer implements MediaPlayer {
+    MediaAdapter mediaAdapter;
+
+    @Override
+    public void play(String audioType, String fileName) {
+        //inbuilt support to play mp3 music files
+        if("mp3".equalsIgnoreCase(audioType)){
+            System.out.println("Playing mp3 file. Name: "+ fileName);
+        }
+        //mediaAdapter is providing support to play other file formats
+        else if("vlc".equalsIgnoreCase(audioType) || "mp4".equalsIgnoreCase(audioType)){
+            mediaAdapter = new MediaAdapter(audioType);
+            mediaAdapter.play(audioType, fileName);
+        }
+        else{
+            System.out.println("Invalid media. " + audioType + " format not supported");
+        }
+    }
+}
