84ac90b412f7a9dec3d9c924020f9d63acc73e15
David Ding <chengjie.ding@gmail.com>
2014-08-20 10:47:36 +0800

    add classes for builder pattern

---
+++ src/main/java/com/demo2do/patterns/builder/ChickenBurger.java
@@ -0,0 +1,13 @@
+package com.demo2do.patterns.builder;
+
+public class ChickenBurger extends Burger {
+    @Override
+    public String name() {
+        return "Chicken Burger";
+    }
+
+    @Override
+    public float price() {
+        return 50.5f;
+    }
+}
---
+++ src/main/java/com/demo2do/patterns/builder/Item.java
@@ -0,0 +1,9 @@
+package com.demo2do.patterns.builder;
+
+public interface Item {
+    public String name();
+
+    public Packing packing();
+
+    public float price();
+}
